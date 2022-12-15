dbaa737fdb9369794f96fa62d31054a2f9e78c2a
1 <1@LAPTOP-01N7APUP>
2022-12-15 08:58:35 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/BeginWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для BeginWindow.xaml
+    /// </summary>
+    public partial class BeginWindow : Window
+    {
+        public BeginWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = this.Owner as MainWindow;
+            if (main != null)
+            {
+                main.TextFromBeginWindow = TextBox1.Text;
+                if(RadioButton1.IsChecked == true)
+                {
+                    main.ToCaseBeginWindow = 1;
+                }
+                else if(RadioButton2.IsChecked == true)
+                {
+                    main.ToCaseBeginWindow = 2;
+                }
+                else
+                {
+                    main.ToCaseBeginWindow = 0;
+                }
+            }
+
+            main.WorkButton.IsEnabled = !string.IsNullOrEmpty(TextBox1.Text);
+
+            this.Close();
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string TextFromBeginWindow { get; set; }
+        public int ToCaseBeginWindow { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AboutWindow aboutWindow = new AboutWindow();
+            aboutWindow.ShowDialog();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            BeginWindow beginWindow = new BeginWindow();
+            beginWindow.Owner = this;
+            beginWindow.ShowDialog();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            switch (ToCaseBeginWindow)
+            {
+                case 0:
+                    MessageBox.Show("Не выбран вариант перевода к верхему или нижнему регистру!");
+                    break;
+                case 1:
+                    MessageBox.Show(TextFromBeginWindow.ToLower());
+                    break;
+                case 2:
+                    MessageBox.Show(TextFromBeginWindow.ToUpper());
+                    break;
+            }
+        }
+    }
+}
